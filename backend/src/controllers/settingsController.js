1e0f03e1b4e68b3d930e648ae3630e97203cb73f
ABabayaga <alef_rdl@hotmail.com>
2025-04-23 21:07:18 +0400

    Finalizando modulo segurança

---
+++ backend/src/controllers/settingsController.js
@@ -0,0 +1,8 @@
+function getSettings(req, res, next) {
+
+    res.json({
+        email: 'contato@alefdevops.com.br1',
+    });
+}
+
+module.exports = { getSettings }
--- frontend/src/public/Login/Login.js
+++ frontend/src/public/Login/Login.js
@@ -20,12 +20,15 @@ function Login() {
         event.preventDefault();
 
         doLogin(email, password)
-            .then(isValid => {
-                if (isValid)
+            .then(response => {
+                if (response) {
+                    localStorage.setItem('token', response.token);
                     history.push('/settings');
+                }
             })
             .catch(err => {
-                setError(err);
+                console.error(err);
+                setError(`Invalid user and/or password`)
             })
 
     }
